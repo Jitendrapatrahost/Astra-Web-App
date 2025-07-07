19e2fa00ca65049c1d89fe51c64d9a6c3f3cbd44
Jitendrapatrahost <jitendrapatra.host@gmail.com>
2025-07-07 09:05:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -454,3 +454,10 @@ function typeEffect() {
 }
 
 typeEffect();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch((error) => console.error("Service Worker Error:", error));
+}
